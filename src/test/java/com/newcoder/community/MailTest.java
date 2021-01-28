1544a543060b1234e371a5a4256e541a806b0711
wuruiwen <2946996247@qq.com>
2021-01-28 09:22:23 +0800

    第二次更新

--- src/test/java/com/newcoder/community/MailTest.java
+++ src/test/java/com/newcoder/community/MailTest.java
@@ -27,11 +27,11 @@ public class MailTest {
         // Context是要传入Template模板引擎的参数
         Context context = new Context();
         context.setVariable("username","大傻逼");
-
+        context.setVariable("id","123456");
         //生成要发送的内容，就是把参数掺进html页面，因为html页面有待要填补的参数username
         String content = templateEngine.process("/mail/demo",context);
         System.out.println("content = " + content);
-        mailClient.sendMail("3176289501@qq.com","HTML_TEST",content);
+        mailClient.sendMail("2946996247@qq.com","HTML_TEST",content);
 
     }
 }
