98daa71ce7e9b3f1e60136ea485394555b7627e0
wuruiwen <2946996247@qq.com>
2021-01-31 15:59:37 +0800

    增加验证码功能

--- src/main/java/com/newcoder/community/controller/LoginController.java
+++ src/main/java/com/newcoder/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.newcoder.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.newcoder.community.entity.User;
 import com.newcoder.community.service.UserService;
 import com.newcoder.community.utils.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @GetMapping("/register")
     public String getRegisterPage(){
@@ -26,6 +40,32 @@ public class LoginController implements CommunityConstant {
     @GetMapping("/login")
     public String getLoginPage(){
         return "/site/login";
+    }
+
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        /**
+         * 将验证码存入session
+         * 验证码包含2个东西，一是用于验证的text，还有就是显示在页面的这个text“加密”后的image，
+         * 显然 text 是用于验证正确与否的，是敏感信息，不能存在客户端，
+         * 要存储在服务端，因此用session
+         */
+        session.setAttribute("kaptcha",text);
+
+        //将图片响应给浏览器
+        response.setContentType("image/png");//设置响应编码，常见有 text/html
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png",os);
+        }catch (IOException e){
+            //e.printStackTrace();
+            logger.error("服务器响应验证码失败！" + e.getMessage());
+        }
     }
 
     @PostMapping("/register")
